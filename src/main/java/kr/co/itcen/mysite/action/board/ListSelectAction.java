c94ebf38a7595550f5dfcea22a53d3f160d508e3
ljy <leejy3653@naver.com>
2019-09-24 21:20:19 +0900

    페이징

--- src/main/java/kr/co/itcen/mysite/action/board/ListSelectAction.java
+++ src/main/java/kr/co/itcen/mysite/action/board/ListSelectAction.java
@@ -18,28 +18,24 @@ public class ListSelectAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		HttpSession session = request.getSession();
-		
+
 		String page = request.getParameter("page");
-		page = (page==null)? "1" : page ;
-		//화면에 띄우 게시글 수
-		int board_count = 10;
-		//화면에 보여줄 페이지
-		int size_page = 5;
-		
+		page = (page == null) ? "1" : page;
+
+		int board_count = 5; // 한페이지에 띄울 글
+		int size_page = 5; // 한페이지에 띄울 리스트 < 1 2 3 4 5 >
+
 		List<BoardVo> list = new BoardDao().getList(Integer.parseInt(page), board_count);
-		//게시물 총갯수
 		int count = new BoardDao().count();
-	
+
 		int start_page = 0;
 		int end_page = 0;
-		//총 페이지
-		int total_page = ((count-1)/board_count)+1;
-		//시작페이지
-		start_page = ((Integer.parseInt(page)-1)/size_page)*size_page+1;
-		//끝페이지
-		end_page = start_page + size_page -1;
-		System.out.println(count);
-		
+
+		int total_page = ((count - 1) / board_count) + 1;// 총 페이지
+
+		start_page = ((Integer.parseInt(page) - 1) / size_page) * size_page + 1;// 시작페이지
+		end_page = start_page + size_page - 1;// 끝페이지
+
 		request.setAttribute("authuser", session.getAttribute("authuser"));
 		request.setAttribute("list", list);
 		request.setAttribute("page", page);
@@ -49,7 +45,7 @@ public class ListSelectAction implements Action {
 		request.setAttribute("total_page", total_page);
 		request.setAttribute("count", count);
 		request.setAttribute("board_count", board_count);
-		
+
 		WebUtils.forward(request, response, "/WEB-INF/views/board/list.jsp");
 
 	}
--- src/main/java/kr/co/itcen/mysite/action/board/SearchSelect.java
+++ src/main/java/kr/co/itcen/mysite/action/board/SearchSelect.java
@@ -18,25 +18,24 @@ public class SearchSelect implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		
+
 		String kwd = request.getParameter("kwd");
 		String page = request.getParameter("page");
-		page = (page==null)? "1" : page ;
-		int board_count = 10;
+		page = (page == null) ? "1" : page;
+		int board_count = 5; // 한페이지당 키워드 검색 노출 게시글 수
 		int size_page = 5;
-		
+
 		List<BoardVo> list = new BoardDao().getList(Integer.parseInt(page), board_count, kwd);
 		int count = new BoardDao().count(kwd);
-		
+
 		int start_page = 0;
 		int end_page = 0;
-		
-		int total_page = ((count-1)/board_count)+1;
-		
-		start_page = ((Integer.parseInt(page)-1)/size_page)*size_page+1;
-		end_page = start_page + size_page -1;
-		
-		
+
+		int total_page = ((count - 1) / board_count) + 1;
+
+		start_page = ((Integer.parseInt(page) - 1) / size_page) * size_page + 1;
+		end_page = start_page + size_page - 1;
+
 		request.setAttribute("authuser", session.getAttribute("authuser"));
 		request.setAttribute("list", list);
 		request.setAttribute("page", page);
@@ -44,13 +43,12 @@ public class SearchSelect implements Action {
 		request.setAttribute("start_page", start_page);
 		request.setAttribute("size_page", size_page);
 		request.setAttribute("total_page", total_page);
-		request.setAttribute("kwd",	kwd);
+		request.setAttribute("kwd", kwd);
 		request.setAttribute("total_page", total_page);
 		request.setAttribute("count", count);
 		request.setAttribute("board_count", board_count);
-		
-		WebUtils.forward(request, response, "/WEB-INF/views/board/list.jsp");
 
+		WebUtils.forward(request, response, "/WEB-INF/views/board/list.jsp");
 
 	}
 
